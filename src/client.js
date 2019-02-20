5a817b6a018644add432a34d5cf0e41a86437a7a
tranquoctrung <tranquoctrung.10k13@gmail.com>
2019-02-20 12:23:26 +0700

    update new carousel nhieu hinh anh con chua xu ly dc phai tu viet

--- src/client.js
+++ src/client.js
@@ -3,37 +3,117 @@
 import React ,{Component } from 'react';
 
 import './client.sass';
-
-import { Container, Row, Col } from 'reactstrap';
-
-class Client_img extends Component {
-    render() {
-        return (
-        <Col sm="6" md="2">
-            <img className="img-fluid" src={this.props.url_img}></img>
-        </Col>
-        );
-    }
     
-}
 
-const url_img  = './img/clients_';
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
+  } from 'reactstrap';
+
+  const items = [
+    {
+      src: './img/clients_1.png',
+      altText: '',
+      caption: ''
+    },
+    {
+      src: './img/clients_2.png',
+      altText: '',
+      caption: ''
+    },
+    {
+      src: './img/clients_3.png',
+      altText: '',
+      caption: ''
+    },
+    {
+      src: './img/clients_4.png',
+      altText: '',
+      caption: ''
+    },
+    {
+      src: './img/clients_5.png',
+      altText: '',
+      caption: ''
+    },
+    {
+      src: './img/clients_6.png',
+      altText: '',
+      caption: ''
+    }
+  ];
 
 class Main_Client extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = { activeIndex: 0 };
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+      }
+    
+      onExiting() {
+        this.animating = true;
+      }
+    
+      onExited() {
+        this.animating = false;
+      }
+    
+      next() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({ activeIndex: nextIndex });
+      }
+    
+      previous() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+        this.setState({ activeIndex: nextIndex });
+      }
+    
+      goToIndex(newIndex) {
+        if (this.animating) return;
+        this.setState({ activeIndex: newIndex });
+      }
+    
+      render() {
+          
+        const { activeIndex } = this.state;
+    
+        const slides = items.map((item) => {
+          return (
+            <CarouselItem
+              onExiting={this.onExiting}
+              onExited={this.onExited}
+              key={item.src}
+            >
+              <img src={item.src} alt={item.altText} />
+              <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+            </CarouselItem>
+         
+          );
+        });
+    
         return (
-            <Container className="mt-3">
-                <Row>
-                    <Client_img url_img={`${url_img}1.png`}/>
-                    <Client_img url_img={`${url_img}2.png`}/>
-                    <Client_img url_img={`${url_img}3.png`}/>
-                    <Client_img url_img={`${url_img}4.png`}/>
-                    <Client_img url_img={`${url_img}5.png`}/>
-                    <Client_img url_img={`${url_img}6.png`}/>
-                </Row>
-            </Container>
+          <Carousel
+            activeIndex={activeIndex}
+            next={this.next}
+            previous={this.previous}
+          >
+            <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+            {slides}
+            <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+            <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+          </Carousel>
+          
         );
+      }
     }
-}
 
 export default Main_Client
--- src/slide_baner.js
+++ src/slide_baner.js
@@ -1,15 +1,98 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, {Component } from 'react';
 import './slide_baner.sass';
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
+  } from 'reactstrap';
+
+  const items = [
+    {
+      src: './img/home_slider_1.jpg',
+      altText: '',
+      caption: ''
+    },
+    {
+      src: './img/hinh01.png',
+      altText: '',
+      caption: ''
+    },
+    {
+      src: './img/hinh02.png',
+      altText: '',
+      caption: ''
+    }
+  ];
 
 class Slide_Baner extends Component {
-    render() {
-        return (
-            <section className="home-slider">
-                <img src="./img/home_slider_1.jpg"></img>
-                <h3 className="text-center">Chúng tôi là FULLSTACK VN hãy cố gắng rèn luyện để trở thành 1 người front-end thực xự</h3>
-
-            </section>
+    constructor(props) {
+        super(props);
+        this.state = { activeIndex: 0 };
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+      }
+            onExiting() {
+            this.animating = true;
+            }
+        
+            onExited() {
+            this.animating = false;
+            }
+        
+            next() {
+            if (this.animating) return;
+            const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+            this.setState({ activeIndex: nextIndex });
+            }
+        
+            previous() {
+            if (this.animating) return;
+            const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+            this.setState({ activeIndex: nextIndex });
+            }
+        
+            goToIndex(newIndex) {
+            if (this.animating) return;
+            this.setState({ activeIndex: newIndex });
+            }
+        
+            render() {
+            const { activeIndex } = this.state;
+        
+            const slides = items.map((item) => {
+                return (
+                <CarouselItem
+                    onExiting={this.onExiting}
+                    onExited={this.onExited}
+                    key={item.src}
+                >
+                    <img src={item.src} alt={item.altText} />
+                    <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+                </CarouselItem>
+                );
+            });
+        
+            return (
+                <section className="slider">
+                <Carousel
+                activeIndex={activeIndex}
+                next={this.next}
+                previous={this.previous}
+                >
+                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+                </Carousel>
+                <h5 className="text-center">Chúng tôi là FULLSTACK VN hãy cố gắng rèn luyện để trở thành 1 người front-end thực xự</h5>
+                </section>
+           
         );
     }
 }
